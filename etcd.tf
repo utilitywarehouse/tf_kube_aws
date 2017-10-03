ddd2569252e84e674dc55e99f395c2ddb3ed953a
Dimitrios Karagiannis <dhkarag@gmail.com>
2017-10-03 18:06:36 +0100

    open etcd-metrics-proxy port to workers

--- etcd.tf
+++ etcd.tf
@@ -126,6 +126,15 @@ resource "aws_security_group_rule" "ingress-master-to-etcd" {
   security_group_id        = "${aws_security_group.etcd.id}"
 }
 
+resource "aws_security_group_rule" "ingress-worker-to-etcd-metrics-proxy" {
+  type                     = "ingress"
+  from_port                = 2381
+  to_port                  = 2381
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.worker.id}"
+  security_group_id        = "${aws_security_group.etcd.id}"
+}
+
 resource "aws_security_group_rule" "etcd-ssh" {
   type                     = "ingress"
   from_port                = 22
