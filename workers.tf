4109e6cc08cc8b580d7f3b33910a7f7189143810
ffilippopoulos <ffilippopoulos@uw.co.uk>
2024-11-14 12:52:20 +0000

    Extend ami parameters logic to catch worker-spot ASG as well

--- workers.tf
+++ workers.tf
@@ -130,7 +130,7 @@ resource "aws_launch_template" "worker" {
 resource "aws_launch_template" "worker_spot" {
   name_prefix = "${var.cluster_name}-worker-spot-"
   iam_instance_profile { name = aws_iam_instance_profile.worker.name }
-  image_id               = var.containerlinux_ami_id
+  image_id               = var.containerlinux_ami_parameter != "" ? var.containerlinux_ami_parameter : var.containerlinux_ami_id
   instance_type          = var.worker_instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.worker.id]
